a7bc6f7aea14228aac55b0ba61335425c7d8d29d
Jay hayes <hayeswj@mail.uc.edu>
2020-09-06 19:08:21 -0400

    Updating the personal website.

--- Wave_inspire15.py
+++ Wave_inspire15.py
@@ -1,46 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from selenium.webdriver.common.keys import Keys
 import time
 
-def send_myself_email():
-    email = 'Enter email name here'
-    password = 'Enter password here'
-    send_to_email = 'Where are you sending the email to'
-    subject = 'Email Subject'
-    message = 'Email Body'
 
-    msg = MIMEMultipart()
-    msg['From'] = email
-    msg['To'] = send_to_email
-    msg['Subject'] = subject
+def get_shoe_price(url):
+    driver.get(url)
+    time.sleep(2)
+    number = len(driver.find_elements_by_id('priceblock_ourprice'))
+    shoe_size = driver.find_elements_by_class_name('a-dropdown-prompt')[0].text
+    if number != 0 and shoe_size is '11.5':
+        price_list.append(float(driver.find_elements_by_id('priceblock_ourprice')[0].text[1:]))
 
-    msg.attach(MIMEText(message, 'plain'))
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(email, password)
-    text = msg.as_string()
-    server.sendmail(email, send_to_email, text)
 
-jack_rabbit_url = 'https://www.jackrabbit.com/brands/mizuno/wave-inspire.html'
-price_point = 67.50
 
-chrome_driver_path = r"path to chromedriver"
+amazon_url1 = 'https://www.amazon.com/Mizuno-Mens-Inspire-Running-Teal-Dress/dp/B07CHTZ7LY/ref=sr_1_1?crid=1NP6MYQ13T6S7&dchild=1&keywords=wave%2Binspire%2B15%2Bmen&qid=1585569820&sprefix=wave%2Binsp%2Caps%2C163&sr=8-1&th=1&psc=1'
+amazon_url2 = 'https://www.amazon.com/Mizuno-Mens-Inspire-Running-Teal-Dress/dp/B07CKJFBN5/ref=sr_1_1?crid=1NP6MYQ13T6S7&dchild=1&keywords=wave%2Binspire%2B15%2Bmen&qid=1585569820&sprefix=wave%2Binsp%2Caps%2C163&sr=8-1&th=1&psc=1'
+amazon_url3 = 'https://www.amazon.com/Mizuno-Mens-Inspire-Running-Teal-Dress/dp/B07HRJ3HQR/ref=sr_1_1?crid=1NP6MYQ13T6S7&dchild=1&keywords=wave%2Binspire%2B15%2Bmen&qid=1585569820&sprefix=wave%2Binsp%2Caps%2C163&sr=8-1&th=1&psc=1'
+amazon_url4 = 'https://www.amazon.com/Mizuno-Mens-Inspire-Running-Teal-Dress/dp/B07HRHQW9H/ref=sr_1_1?crid=1NP6MYQ13T6S7&dchild=1&keywords=wave%2Binspire%2B15%2Bmen&qid=1585569820&sprefix=wave%2Binsp%2Caps%2C163&sr=8-1&th=1&psc=1'
+amazon_url5 = 'https://www.amazon.com/Mizuno-Mens-Inspire-Running-Teal-Dress/dp/B07CHV7B95/ref=sr_1_1?crid=1NP6MYQ13T6S7&dchild=1&keywords=wave%2Binspire%2B15%2Bmen&qid=1585569820&sprefix=wave%2Binsp%2Caps%2C163&sr=8-1&th=1&psc=1'
+amazon_url6 = 'https://www.amazon.com/Mizuno-Mens-Inspire-Running-Teal-Dress/dp/B07CKJ6TKM/ref=sr_1_1?crid=1NP6MYQ13T6S7&dchild=1&keywords=wave%2Binspire%2B15%2Bmen&qid=1585569820&sprefix=wave%2Binsp%2Caps%2C163&sr=8-1&th=1&psc=1'
+price_point = 70.50
+price_list = []
+
+chrome_driver_path = r"path/to/chromedriver"
 options = Options()
 options.headless = True
 # options.headless = False
 driver = webdriver.Chrome(chrome_driver_path, options=options)
-driver.get(jack_rabbit_url)
-time.sleep(2)
-driver.find_elements_by_class_name('bx-button')[1].click()
-
-for items in driver.find_elements_by_class_name('price'):
-    if items.text is not '' and (len(items.text[1:]) < 6):
-        jack_rabbit_price = float(items.text[1:])
-        if jack_rabbit_price < price_point:
-            #If the shoes are below the price point I will send myself an email
-            send_myself_email()
+get_shoe_price(amazon_url1)
+get_shoe_price(amazon_url2)
+get_shoe_price(amazon_url3)
+get_shoe_price(amazon_url4)
+get_shoe_price(amazon_url5)
+get_shoe_price(amazon_url6)
 driver.close()
+
+for items in price_list:
+    if items < price_point:
+        print('Mizuno Wave Inspire are on sale for $' + str(items))
+
+print('Script has finished executing.')
